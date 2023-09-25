879afbf57fdc291ee0b0c5c4a2565423f644222b
Flagelo6482 <frankln6482@gmail.com>
2023-09-25 23:41:02 +0000

    Feat: New Proyect :sparkles:

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const fs = require('fs');
+const util = require('util');
+
+function readAndPrintFileContent(filePath) {
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(util.inspect(err, false, null, true));
+      return;
+    }
+    console.log(data);
+  });
+}
+
+// Example usage: node script.js file.txt
+const filePath = process.argv[2];
+readAndPrintFileContent(filePath);
