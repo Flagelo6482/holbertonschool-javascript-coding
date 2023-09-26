8e839bed6b4e00804c33a19b706a8f5b4f5b435a
Flagelo6482 <frankln6482@gmail.com>
2023-09-26 14:22:57 +0000

    Feat: Update variable :rocket:

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-//Argumento: archivo para escribir
+// Argumento: archivo para escribir
 const path = process.argv[2];
-//Argumento: cadena para pasar al archivo
+// Argumento: cadena para pasar al archivo
 const str = process.argv[3];
 
 const fs = require('fs');
 const util = require('util');
-
 
 fs.writeFile (path, str,'utf8', (err) => {
     if (err) {
