7f1feb0a58ac5ba4c72fa4ce3c8e51788ebaaa34
Flagelo6482 <frankln6482@gmail.com>
2023-09-26 14:40:28 +0000

    Feat: Update indentation :rocket:

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -3,8 +3,8 @@ const URL = process.argv[2];
 const request = require('request');
 
 request.get(URL, (err, response) => {
-    if (err) {
-        console.error(`code: ${err}`);
-    }
-    console.log(`code: ${response.statusCode}`);
+  if (err) {
+    console.error(`code: ${err}`);
+  }
+  console.log(`code: ${response.statusCode}`);
 });
